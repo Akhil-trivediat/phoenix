3076602c81a7c0a18a37693611dfc14db81b4369
kiranb <kerankousyal@gmail.com>
2021-05-17 21:14:22 -0400

    api documentation

--- src/app/pages/api-documentation/api-documentation.component.ts
+++ src/app/pages/api-documentation/api-documentation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const SwaggerUIBundle: any;
+
 @Component({
   selector: 'app-api-documentation',
   templateUrl: './api-documentation.component.html',
@@ -9,7 +11,18 @@ export class APIDocumentationComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const ui = SwaggerUIBundle({
+      dom_id: '#swagger-ui',
+      layout: 'BaseLayout',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+      ],
+      url: 'https://pheonixapidoc.s3.us-east-2.amazonaws.com/PheonixSPA_API-dev-swagger.json',
+      docExpansion: 'none',
+      operationsSorter: 'alpha'
+    });
   }
 
 }

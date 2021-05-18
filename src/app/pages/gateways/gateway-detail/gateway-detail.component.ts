67d679e14eb5c4ab667371b3072cb6f984a4ae8d
Suryasnata Mohanty <suryasnata@trivediat.com>
2021-05-18 18:29:12 -0400

    added content for notification screen

--- src/app/pages/gateways/gateway-detail/gateway-detail.component.ts
+++ src/app/pages/gateways/gateway-detail/gateway-detail.component.ts
@@ -586,7 +586,7 @@ export class GatewayDetailComponent implements OnInit {
     }
 
     let IOTParams = {
-      topic: "config_sub_tt_message",
+      topic: this.gatewayid + "/config_sub_tt_message",
       payload: deviceConfigJSON
     }
 
